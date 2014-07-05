2a3553948be9ebf5117e214fbcc7ac39e5b602fd
Kevin Bost <kevbost@gmail.com>
2014-07-05 14:22:49 -0400

    fully functional, input validation, Y-axis spacing between dataPoints

--- app/scripts/paper.js
+++ app/scripts/paper.js
@@ -73,20 +73,18 @@ $('.go').click(function(){
 		strokeColor: [0.8], // 80% black:
 		strokeCap: 'square',
 		// fullySelected: true
-	});
-
-	var referencePath = new Path({
-		strokeColor: [0.8], // 80% black:
-		strokeCap: 'square',
-		// fullySelected: true
-	});
-
-	var style = {
-		fillColor: 'red',
-		strokeColor: [0.8],
-		strokeWidth: 2,
-		fontSize: 20
-	};
+		});
+		var referencePath = new Path({
+			strokeColor: [0.8], // 80% black:
+			strokeCap: 'square',
+			// fullySelected: true
+			});
+			var style = {
+				fillColor: 'red',
+				strokeColor: [0.8],
+				strokeWidth: 2,
+				fontSize: 20
+				};
 // =======================================
 // Take date inputs and draw the timeline
 // =======================================
@@ -99,6 +97,7 @@ $('.go').click(function(){
 	var startYear = parseInt(startYearString);
 	var endYear = parseInt(endYearString);
 	var dateRange = (endYear - startYear);
+
 
 // =======================================================================
 console.log('%cThe span of dates being viewed is ' + (dateRange - 1) + ' years.', 'font-size:20px;color:green');
@@ -190,80 +189,145 @@ console.log('%cStartYear is ' + startYear, 'font-size:20px;color:green')
 
 
 
-// =======================================================================
-// Add new dataPoint to graph
+
+// ADD NEW DATA POINT
 // =======================================================================
 	$('.add').click(function(){
-		width = view.size.width;
+// =============================================
+	width = view.size.width;
+// Set clickNum to 50, and each time $('.add') is clicked, +50 to clickNum
+clickNums()
+	function clickNums () {
+		clickNum = $(this).data('clickNum');
+			if (!clickNum) clickNum = 50;
+				$(this).data('clickNum', clickNum += 20);
+		checkInputs()
+	}
+// =============================================
+
+	function checkInputs() {
+
 
 		// console.clear();
 		$('.dateRange').addClass('activated');
 		$('.dataPoint').addClass('activated');
-		dataPointStartString = $('.dataPoint-start-input').val();
-		dataPointEndString = $('.dataPoint-end-input').val();
-		var style = {
-			fillColor: 'red',
-			strokeColor: 'black',
-			strokeWidth: 2,
-			fontSize: 20
-		};
 
-		var dataPointStart	= parseInt(dataPointStartString);
-		var dataPointEnd	= parseInt(dataPointEndString);
-		var dataPointRange	= dataPointEnd - dataPointStart;
+			dataPointName 			= $('.dataPoint-name-input').val();
+			dataPointStartString 	= $('.dataPoint-start-input').val();
+			dataPointEndString 		= $('.dataPoint-end-input').val();
+				dataPointStart		= parseInt(dataPointStartString);
+				dataPointEnd		= parseInt(dataPointEndString);
+				dataPointRange		= dataPointEnd - dataPointStart;
+				checkName()
+
+		function checkName () {
+			if(dataPointName.trim() == "") { // note the trim for eliminating empty spaces
+				alert('Please enter a name of your new data point.');
+				// return;
+				} else { checkInputs() }
+			}
+
+		// function checkStart () {
+		// 	if(dataPointStartString.trim() == "") {
+		// 		alert('Need beginning year.');
+		// 		// return;
+		// 		} else if(dataPointStart < 1000) {
+		// 			alert('Year needs to match YYYY')
+		// 			} else if(dataPointStart < startYear) {
+		// 				alert('START should be after the graph\'s start year.')
+		// 				} else if(dataPointStart > dataPointEnd) {
+		// 					alert('START needs to be a year before END')
+		// 					} else { checkEnd() }
+		// 		}
+
+		// function checkEnd () {
+		// 	if(dataPointEndString.trim() == "") {
+		// 		alert('Need ending year.');
+		// 		// return;
+		// 		} else if(dataPointEnd < 1000) {
+		// 			alert('END needs to match YYYY')
+		// 			} else if(dataPointEnd > endYear) {
+		// 				alert('END should be before the graph\'s end year.')
+		// 				} else { renderData() }
+		// 			}
+
+		function checkInputs(){
+			switch (true)
+				{
+				// Check Start Input
+				// =============================================
+				case (dataPointStartString.trim() == ""):
+				alert('Need beginning year.');
+				$(this).data('clickNum', clickNum -= 20);
+				break;
+					case (dataPointStart < 1000):
+					alert('Year needs to match YYYY');
+					$(this).data('clickNum', clickNum -= 20);
+					break;
+						case (dataPointStart < startYear):
+						alert('START should be after the graph\'s start year.');
+						$(this).data('clickNum', clickNum -= 20);
+						break;
+							case (dataPointStart > dataPointEnd):
+							alert('START needs to be a year before END');
+							$(this).data('clickNum', clickNum -= 20);
+							break;
+				// =============================================
+
+				// Check End Input
+				// =============================================
+				case (dataPointEndString.trim() == ""):
+				alert('Need ending year.');
+				$(this).data('clickNum', clickNum -= 20);
+				break;
+					case (dataPointEnd < 1000):
+					alert('END needs to match YYYY');
+					$(this).data('clickNum', clickNum -= 20);
+					break;
+						case (dataPointEnd > endYear):
+						alert('END should be before the graph\'s end year.');
+						$(this).data('clickNum', clickNum -= 20);
+						break;
+							// case ():
+							// // statement
+							// break;
+				// =============================================
+				default: 
+				renderData();
+				}
+			}
+		}
 
 
-//    .-')     ('-.     ('-.        .-. .-')    ('-.                         (`\ .-') /`   
-//   ( OO ). _(  OO)  _(  OO)       \  ( OO ) _(  OO)                         `.( OO ),'   
-//  (_)---\_|,------.(,------.       ;-----.\(,------.,--.     .-'),-----. ,--./  .--.     
-//  /    _ | |  .---' |  .---'       | .-.  | |  .---'|  |.-')( OO'  .-.  '|      |  |     
-//  \  :` `. |  |     |  |           | '-' /_)|  |    |  | OO )   |  | |  ||  |   |  |,    
-//   '..`''.||  '--. (|  '--.        | .-. `.(|  '--. |  |`-' \_) |  |\|  ||  |.'.|  |_)   
-//  .-._)   \|  .--'  |  .--'        | |  \  ||  .--'(|  '---.' \ |  | |  ||         |     
-//  \       /|  `---. |  `---.       | '--'  /|  `---.|      |   `'  '-'  '|   ,'.   |     
-//   `-----' `------' `------'       `------' `------'`------'     `-----' '--'   '--'     
-                                                                                   
 
+
+
+	// function dataInputs () {
+		function renderData(){
+		console.log(clickNum)
+
+
+		// Use clickNum to change Y location of data point
+		// =============================================
 		var dataPointRangeCounter = dataPointRange;
 		var dataPointRect = new Rectangle(
-							new Point(0, (view.bounds.bottom - 100)), 
-							new Size(((width / dateRange) * dataPointRange), 10)
-						);
-			var path = new Path.Rectangle(dataPointRect);
-				path.fillColor = {
-				hue: Math.random() * 360,
-				saturation: 1,
-				brightness: 1
-				};
+							new Point((width / dateRange) * (dataPointStart - startYear), (view.bounds.bottom - clickNum)), 
+							new Size(((width / dateRange) * dataPointRange), 14)
+							);
 
+							var path = new Path.Rectangle(dataPointRect);
+								path.fillColor = {
+								hue: Math.random() * 360,
+								saturation: 1,
+								brightness: 1
+								};
 
-//    .-')     ('-.     ('-.           ('-.    .-. .-')                    (`-.     ('-.   
-//   ( OO ). _(  OO)  _(  OO)         ( OO ).-.\  ( OO )                 _(OO  )_ _(  OO)  
-//  (_)---\_|,------.(,------.        / . --. / ;-----.\ .-'),-----. ,--(_/   ,. (,------. 
-//  /    _ | |  .---' |  .---'        | \-.  \  | .-.  |( OO'  .-.  '\   \   /(__/|  .---' 
-//  \  :` `. |  |     |  |          .-'-'  |  | | '-' /_)   |  | |  | \   \ /   / |  |     
-//   '..`''.||  '--. (|  '--.        \| |_.'  | | .-. `.\_) |  |\|  |  \   '   /,(|  '--.  
-//  .-._)   \|  .--'  |  .--'         |  .-.  | | |  \  | \ |  | |  |   \     /__)|  .--'  
-//  \       /|  `---. |  `---.        |  | |  | | '--'  /  `'  '-'  '    \   /    |  `---. 
-//   `-----' `------' `------'        `--' `--' `------'     `-----'      `-'     `------' 
-
-
-//     _              _   _   _                                  _   _     _       
-//    / \   _ __   __| | | |_| |__   ___ _ __    ___  ___  ___  | |_| |__ (_)___ _ 
-//   / _ \ | '_ \ / _` | | __| '_ \ / _ | '_ \  / __|/ _ \/ _ \ | __| '_ \| / __(_)
-//  / ___ \| | | | (_| | | |_| | | |  __| | | | \__ |  __|  __/ | |_| | | | \__ \_ 
-// /_/   \_|_| |_|\__,_|  \__|_| |_|\___|_| |_| |___/\___|\___|  \__|_| |_|_|___(_)
-// This is my breakdown of the data i cant figure out. (ie: x)
-// ================================================================================================
-	// var foo = new Rectangle(new Point(x,y), new Size(width, height))
-	// var foo = new Rectangle(new Point( x, view.bounds.bottom - 100 ), new Size(width, 10))
-	// var foo = new Rectangle(new Point( x, view.bounds.bottom - 100 ), new Size(((width / dateRange)*datePointRange), 10))
-// ================================================================================================                                                                                
-
-
-
-
-
+		var dataPointNameListing = new PointText(0, (view.bounds.bottom - clickNum));
+			dataPointNameListing.content = dataPointName;
+			dataPointNameListing.fillColor = 'white';
+			// dataPointNameListing.rotate(300);
+			// dataPointNameListing.position.x -=14;
+			dataPointNameListing.position.y +=11;
 
 // ================================================================================================
 		console.log('%cData Start = ' + dataPointStart, 'font-size:20px;background-color:yellow');
@@ -275,11 +339,12 @@ console.log('%cStartYear is ' + startYear, 'font-size:20px;color:green')
 		// $('.dataPoint-start-input').val('');
 		// $('.dataPoint-end-input').val('');
 // =================================================
-// =========================
 // Redraw canvas after click
 // =========================
 	paper.view.draw();
-
+// =========================
+		}
+	// }
 	});
 });
 
@@ -291,3 +356,28 @@ console.log('%cStartYear is ' + startYear, 'font-size:20px;color:green')
 		// paper.view.update();
 	}
 // =======================================================================
+
+//    .-')     ('-.     ('-.        .-. .-')    ('-.                         (`\ .-') /`   
+//   ( OO ). _(  OO)  _(  OO)       \  ( OO ) _(  OO)                         `.( OO ),'   
+//  (_)---\_|,------.(,------.       ;-----.\(,------.,--.     .-'),-----. ,--./  .--.     
+//  /    _ | |  .---' |  .---'       | .-.  | |  .---'|  |.-')( OO'  .-.  '|      |  |     
+//  \  :` `. |  |     |  |           | '-' /_)|  |    |  | OO )   |  | |  ||  |   |  |,    
+//   '..`''.||  '--. (|  '--.        | .-. `.(|  '--. |  |`-' \_) |  |\|  ||  |.'.|  |_)   
+//  .-._)   \|  .--'  |  .--'        | |  \  ||  .--'(|  '---.' \ |  | |  ||         |     
+//  \       /|  `---. |  `---.       | '--'  /|  `---.|      |   `'  '-'  '|   ,'.   |     
+//   `-----' `------' `------'       `------' `------'`------'     `-----' '--'   '--'     
+//    .-')     ('-.     ('-.           ('-.    .-. .-')                    (`-.     ('-.   
+//   ( OO ). _(  OO)  _(  OO)         ( OO ).-.\  ( OO )                 _(OO  )_ _(  OO)  
+//  (_)---\_|,------.(,------.        / . --. / ;-----.\ .-'),-----. ,--(_/   ,. (,------. 
+//  /    _ | |  .---' |  .---'        | \-.  \  | .-.  |( OO'  .-.  '\   \   /(__/|  .---' 
+//  \  :` `. |  |     |  |          .-'-'  |  | | '-' /_)   |  | |  | \   \ /   / |  |     
+//   '..`''.||  '--. (|  '--.        \| |_.'  | | .-. `.\_) |  |\|  |  \   '   /,(|  '--.  
+//  .-._)   \|  .--'  |  .--'         |  .-.  | | |  \  | \ |  | |  |   \     /__)|  .--'  
+//  \       /|  `---. |  `---.        |  | |  | | '--'  /  `'  '-'  '    \   /    |  `---. 
+//   `-----' `------' `------'        `--' `--' `------'     `-----'      `-'     `------' 
+//     _              _   _   _                                  _   _     _       
+//    / \   _ __   __| | | |_| |__   ___ _ __    ___  ___  ___  | |_| |__ (_)___ _ 
+//   / _ \ | '_ \ / _` | | __| '_ \ / _ | '_ \  / __|/ _ \/ _ \ | __| '_ \| / __(_)
+//  / ___ \| | | | (_| | | |_| | | |  __| | | | \__ |  __|  __/ | |_| | | | \__ \_ 
+// /_/   \_|_| |_|\__,_|  \__|_| |_|\___|_| |_| |___/\___|\___|  \__|_| |_|_|___(_)
+// This is my breakdown of the data i cant figure out. (ie: x)
